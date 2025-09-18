4e55b5fc04705e27e22bd554989719dfa7259433
stivcode21 <stivenelcodificador@gmail.com>
2025-09-17 19:42:52 -0500

    feat: new exercise number 5

---
+++ ejercicio-5.py
@@ -0,0 +1,7 @@
+#5 Promedio de 3 notas
+nota1 = int(input("ingresa nota 1: "))
+nota2 = int(input("ingresa nota 2: "))
+nota3 = int(input("ingresa nota 3: "))
+suma = nota1 + nota2 + nota3
+resultado = suma / 3
+print("tus promedio de ")
