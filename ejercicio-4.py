5b90350e630b9472cd1fe808e18ca377aa2be264
stivcode21 <stivenelcodificador@gmail.com>
2025-09-17 19:42:42 -0500

    feat: new exercise number 4

---
+++ ejercicio-4.py
@@ -0,0 +1,6 @@
+#4 total a pagar por 3 productos
+precio1 = float(input("ingresa precio pruducto #1: "))
+precio2 = float(input("ingresa precio pruducto #2: "))
+precio3 = float(input("ingresa precio pruducto #3: "))
+precioTotal = precio1 + precio2 + precio3
+print("Tu precio total es:", precioTotal)
