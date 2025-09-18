eac6918c40199297b861d1687901f18a1c1bd293
stivcode21 <stivenelcodificador@gmail.com>
2025-09-17 19:37:04 -0500

    feat: exercise 1

---
+++ ejercicio-1.py
@@ -0,0 +1,5 @@
+#1. Suma de los dos numero
+numero1 = int(input("ingresa numero 1: "))
+numero2 = int(input("ingresa numero 2: "))
+resultado = numero1 + numero2
+print("la suma de los numero es:", resultado)
