609520da575303612f6f5b2cf32ff1309da01dc2
stivcode21 <stivenelcodificador@gmail.com>
2025-10-22 21:55:58 -0500

    feat: solve problem #3 of exercise 17

--- ejercicio-17.py
+++ ejercicio-17.py
@@ -12,14 +12,26 @@
 
 #2 === ingresa 5 numeros y calcular la suma total ===
 
-numeros = []
+# numeros = []
 
-for i in range(1, 6):
-    valor = int(input(f"numero {i}:"))
-    numeros.append(valor)
+# for i in range(1, 6):
+#     valor = int(input(f"numero {i}:"))
+#     numeros.append(valor)
 
-totalSuma = 0
-for numero in numeros:
-    totalSuma += numero
+# totalSuma = 0
+# for numero in numeros:
+#     totalSuma += numero
     
-print(f"\nSuma total: {totalSuma}")
+# print(f"\nSuma total: {totalSuma}")
+
+#3 === leer las edades de 6 personas y sacar el promedio ===
+
+edades = []
+sumaTotal = 0
+
+for i in range(1, 7):
+    edad = int(input(f"edad {i}:"))
+    edades.append(edad)
+
+for edad in edades:
+    sumaTotal += edad
