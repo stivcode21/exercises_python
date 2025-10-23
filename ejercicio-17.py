968dcce80de5807806fa67e76c0ed063f163e49d
stivcode21 <stivenelcodificador@gmail.com>
2025-10-22 21:55:01 -0500

    feat: solve problem #2 of exercise 17

--- ejercicio-17.py
+++ ejercicio-17.py
@@ -1,11 +1,25 @@
 #1 === arreglo con 5 nombres y mostrar cada uno en pantalla ===
 
-nombres = []
+# nombres = []
+
+# for i in range(1, 6):
+#     nombre = input(f"nombre {i}:")
+#     nombres.append(nombre)
+
+# print("\nLISTA DE NOMBRES:")
+# for nombre in nombres:
+#     print(nombre)
+
+#2 === ingresa 5 numeros y calcular la suma total ===
+
+numeros = []
 
 for i in range(1, 6):
-    nombre = input(f"nombre {i}:")
-    nombres.append(nombre)
+    valor = int(input(f"numero {i}:"))
+    numeros.append(valor)
 
-print("\nLISTA DE NOMBRES:")
-for nombre in nombres:
-    print(nombre)
+totalSuma = 0
+for numero in numeros:
+    totalSuma += numero
+    
+print(f"\nSuma total: {totalSuma}")
