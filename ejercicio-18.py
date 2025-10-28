2a2aa0b2dddb802d8953dc0c567696fde573048e
stivcode21 <stivenelcodificador@gmail.com>
2025-10-27 22:04:32 -0500

    feat: solve problem #4 in exercise 18

--- ejercicio-18.py
+++ ejercicio-18.py
@@ -2,23 +2,38 @@
 
 #1 Diccionario de persona
 
-persona = {
-    "nombre": "juanito",
-    "edad": "29",
-    "ciudad": "manizales"
-}
+# persona = {
+#     "nombre": "juanito",
+#     "edad": "29",
+#     "ciudad": "manizales"
+# }
 
-print(persona["nombre"])
-print(persona.get("edad"))
+# print(persona["nombre"])
+# print(persona.get("edad"))
 
 #2 acceder y modificar valores
 
-persona["ciudad"] = "medellin"
-persona["profesion"] = "arquitecto" 
+# persona["ciudad"] = "medellin"
+# persona["profesion"] = "arquitecto" 
 
-print(persona)
+# print(persona)
 
 #3 contar claves
 
-totalClaves = len(persona)
-print(totalClaves)
+# totalClaves = len(persona)
+# print(totalClaves)
+
+#4 lista de estudiantes
+
+estudiantes = [
+    {"nombre": "juanito", "nota": 4.5},
+    {"nombre": "carlos", "nota": 3.5},
+    {"nombre": "maria", "nota": 3.0},
+    ]
+
+for estudiante in estudiantes:
+    nota = estudiante["nota"]
+
+    if nota >= 3.5:
+        nombreEstudiante = estudiante["nombre"]
+        print(nombreEstudiante)
