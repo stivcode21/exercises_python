0adadb4988ded3760cf045c25b502e31811fa459
stivcode21 <stivenelcodificador@gmail.com>
2025-10-27 22:01:48 -0500

    feat: solve problem #1 in exercise18

---
+++ ejercicio-18.py
@@ -0,0 +1,13 @@
+#TALLER DICCIONARIOS
+
+#1 Diccionario de persona
+
+persona = {
+    "nombre": "juanito",
+    "edad": "29",
+    "ciudad": "manizales"
+}
+
+print(persona["nombre"])
+print(persona.get("edad"))
+
