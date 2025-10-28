ca445b5b05e249bdb2b0dc381505d3859f18e3f7
stivcode21 <stivenelcodificador@gmail.com>
2025-10-27 22:03:43 -0500

    feat: solve problem #2 and #3 in exercise18

--- ejercicio-18.py
+++ ejercicio-18.py
@@ -11,3 +11,14 @@ persona = {
 print(persona["nombre"])
 print(persona.get("edad"))
 
+#2 acceder y modificar valores
+
+persona["ciudad"] = "medellin"
+persona["profesion"] = "arquitecto" 
+
+print(persona)
+
+#3 contar claves
+
+totalClaves = len(persona)
+print(totalClaves)
